82250493a087b9f4e7fd521130ab286b6b47ae03
Farinaz Zahiri <163356393+FarinazZah@users.noreply.github.com>
2025-04-11 20:59:08 -0400

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,238 @@
+import java.util.NoSuchElementException;
+
+/**
+ * Your implementation of an ArrayList.
+ *
+ * @author Farinaz Zahiri
+ * @version 1.0
+ * @userid fzahiri6
+ * @GTID 903687032
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class ArrayList<T> {
+
+    /**
+     * The initial capacity of the ArrayList.
+     *
+     * DO NOT MODIFY THIS VARIABLE!
+     */
+    public static final int INITIAL_CAPACITY = 9;
+
+    // Do not add new instance variables or modify existing ones.
+    private T[] backingArray;
+    private int size;
+
+    /**
+     * Constructs a new ArrayList.
+     *
+     * Java does not allow for regular generic array creation, so you will have
+     * to cast an Object[] to a T[] to get the generic typing.
+     */
+    public ArrayList() {
+        // Want an array with size 0 & the initial capacity value.
+        // Backing array, where the info. actually ends up going
+        // No this. necessary because no potential aliasing
+        size = 0; // Default size
+        // Create backing array with the default initial capacity
+        // Have to cast Object --> Generic
+        backingArray = (T[]) (new Object[INITIAL_CAPACITY]);
+    }
+
+    /**
+     * Adds the element to the specified index.
+     *
+     * Remember that this add may require elements to be shifted.
+     *
+     * Must be amortized O(1) for index size and O(n) for all other cases.
+     *
+     * @param index the index at which to add the new element
+     * @param data  the data to add at the specified index
+     * @throws java.lang.IndexOutOfBoundsException if index < 0 or index > size
+     * @throws java.lang.IllegalArgumentException  if data is null
+     */
+    public void addAtIndex(int index, T data) {
+        // Check if backingArray is at size
+        // backingArray upon first initialization is the same
+        // length as backingArray
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("The index is out of bounds.");
+        }
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null.");
+        }
+        if (backingArray.length == size) {
+            // Want to double size of backingArray, but DO NOT change
+            // INITIAL_CAPACITY
+            T[] currentArray = (T[]) (new Object[backingArray.length * 2]);
+            for (int i = 0; i < backingArray.length; i++) {
+                currentArray[i] = backingArray[i];
+            }
+            backingArray = currentArray; // Successfully doubled size
+        }
+        if (index == size) {
+            backingArray[index] = data;
+        } else {
+            for (int i = size; i >= index; i--) {
+                backingArray[i + 1] = backingArray[i];
+            }
+            backingArray[index] = data;
+        }
+        size++;
+    }
+
+    /**
+     * Adds the element to the front of the list.
+     *
+     * Remember that this add may require elements to be shifted.
+     *
+     * Must be O(n).
+     *
+     * @param data the data to add to the front of the list
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addToFront(T data) {
+        addAtIndex(0, data);
+    }
+
+    /**
+     * Adds the element to the back of the list.
+     *
+     * Must be amortized O(1).
+     *
+     * @param data the data to add to the back of the list
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addToBack(T data) {
+        addAtIndex(size, data);
+    }
+
+    /**
+     * Removes and returns the element at the specified index.
+     *
+     * Remember that this remove may require elements to be shifted.
+     *
+     * Must be O(1) for index size - 1 and O(n) for all other cases.
+     *
+     * @param index the index of the element to remove
+     * @return the data formerly located at the specified index
+     * @throws java.lang.IndexOutOfBoundsException if index < 0 or index >= size
+     */
+    public T removeAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bounds.");
+        }
+        T removedElement = backingArray[index];
+        if (index == (size - 1)) {
+            backingArray[index] = null;
+        } else {
+            backingArray[index] = null;
+            for (int i = index + 1; i < size; i++) {
+                backingArray[i - 1] = backingArray[i];
+            }
+            backingArray[size - 1] = null;
+        }
+        size--;
+        return removedElement;
+    }
+
+    /**
+     * Removes and returns the first element of the list.
+     *
+     * Remember that this remove may require elements to be shifted.
+     *
+     * Must be O(n).
+     *
+     * @return the data formerly located at the front of the list
+     * @throws java.util.NoSuchElementException if the list is empty
+     */
+    public T removeFromFront() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Cannot remove from empty list.");
+        }
+        return removeAtIndex(0);
+    }
+
+    /**
+     * Removes and returns the last element of the list.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the back of the list
+     * @throws java.util.NoSuchElementException if the list is empty
+     */
+    public T removeFromBack() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Cannot remove from empty list.");
+        }
+        return removeAtIndex(size - 1);
+    }
+
+    /**
+     * Returns the element at the specified index.
+     *
+     * Must be O(1).
+     *
+     * @param index the index of the element to get
+     * @return the data stored at the index in the list
+     * @throws java.lang.IndexOutOfBoundsException if index < 0 or index >= size
+     */
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bounds.");
+        }
+        return backingArray[index];
+    }
+
+    /**
+     * Returns whether or not the list is empty.
+     *
+     * Must be O(1).
+     *
+     * @return true if empty, false otherwise
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Clears the list.
+     *
+     * Resets the backing array to a new array of the initial capacity and
+     * resets the size.
+     *
+     * Must be O(1).
+     */
+    public void clear() {
+        size = 0;
+        backingArray = (T[]) (new Object[INITIAL_CAPACITY]);
+    }
+
+    /**
+     * Returns the backing array of the list.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the backing array of the list
+     */
+    public T[] getBackingArray() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingArray;
+    }
+
+    /**
+     * Returns the size of the list.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the list
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
